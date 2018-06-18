1a6a580ad72ea3cfa939bfc4fe78249224189215
George Cave <gcave@stablecoder.ca>
2018-06-17 23:11:56 -0400

    Added new file for extra compiler options
    Currently allows enabline all warnings for the main three compilers.

---
+++ compiler-options.cmake
@@ -0,0 +1,31 @@
+# Copyright 2018 Stable Tec
+# 
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+# 
+#     http://www.apache.org/licenses/LICENSE-2.0
+# 
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+option(ENABLE_ALL_WARNINGS "Compile with all warnings for the major compilers." OFF)
+
+if(ENABLE_ALL_WARNINGS)
+    if (CMAKE_COMPILER_IS_GNUCXX)
+        # GCC
+        set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} -Wall -Weffc++ -Wextra")
+        set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -Wall -Weffc++ -Wextra")
+    elseif("${CMAKE_C_COMPILER_ID}" MATCHES "(Apple)?[Cc]lang" OR "${CMAKE_CXX_COMPILER_ID}" MATCHES "(Apple)?[Cc]lang")
+        # Clang
+        set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} -Wall -Weffc++ -Wextra")
+        set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -Wall -Weffc++ -Wextra")
+    elseif(MSVC)
+        # MSVC
+        set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} /W4")
+        set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} /W4")
+    endif()
+endif()

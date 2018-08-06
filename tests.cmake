2efbef03ecea9bf9670a61ddea6dd4388475aed7
George Cave <gcave@stablecoder.ca>
2018-08-06 14:36:29 -0400

    Modified tests macro and Catch header inclusion
    Now searches for the catch2 directory as well as the original catch
    directory. Also searches for Catch headers on initial configuration
    instead of after the BUILD_TESTS option is enabled. This means one less
    configuration step required, as well as showing if one is found before
    attempting to enable tests.
    
    Renamed the cloning target from 'catch2' to 'git_catch', for clarity.

--- tests.cmake
+++ tests.cmake
@@ -16,23 +16,23 @@
 option(BUILD_TESTS "Build the test programs." OFF)
 option(FORCE_CATCH_CLONE "Forces cloning of the Catch test headers rather than using local." OFF)
 
+find_file(HAVE_CATCH_HPP catch.hpp PATH_SUFFIXES catch2 catch)
+
 macro(_BuildTests)
     if(BUILD_TESTS)
         if(NOT TARGET catch)
             add_library(catch INTERFACE)
-            
-            find_file(HAVE_CATCH_HPP catch.hpp PATH_SUFFIXES catch)
 
             if(NOT HAVE_CATCH_HPP OR FORCE_CATCH_CLONE)
                 # Cloning
-                message(STATUS "No Catch header detected, cloning via Git.")
+                message(STATUS "No local Catch header detected, cloning via Git.")
                 include(ExternalProject)
                 find_package(Git REQUIRED)
 
                 if(CMAKE_CXX_STANDARD AND CMAKE_CXX_STANDARD GREATER 10)
                     message(STATUS "Detected C++11 or better, using Catch2")
                     ExternalProject_Add(
-                        catch2
+                        git_catch
                         PREFIX ${CMAKE_BINARY_DIR}/catch2
                         GIT_REPOSITORY https://github.com/catchorg/Catch2.git
                         TIMEOUT 10
@@ -45,7 +45,7 @@ macro(_BuildTests)
                 else()
                     message(STATUS "Did not detect C++11 or better, using Catch1")
                     ExternalProject_Add(
-                        catch2
+                        git_catch
                         PREFIX ${CMAKE_BINARY_DIR}/catch1
                         GIT_REPOSITORY https://github.com/catchorg/Catch2.git
                         GIT_TAG Catch1.x
@@ -58,8 +58,8 @@ macro(_BuildTests)
                     )
                 endif()
 
-                ExternalProject_Get_Property(catch2 source_dir)
-                add_dependencies(catch catch2)
+                ExternalProject_Get_Property(git_catch source_dir)
+                add_dependencies(catch git_catch)
                 target_include_directories(catch INTERFACE ${source_dir}/single_include  ${source_dir}/single_include/catch2)
             else()
                 # Using Local

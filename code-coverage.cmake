a42e0c36d83f4ee678285927557d5bc461d8111d
George Cave <gcave@stablecoder.ca>
2018-10-14 23:48:47 -0600

    Reworked code coverage for flexibility
    Code coverage has previously worked on the basis of being enabled on an
    all-or-nothing basis, for all targets or none.
    
    This was problematic for cases where coverage information was wanted
    only for particular targets, or for libraries but not the test programs
    testing them.
    
    To alleviate this, the macros have been revised. to allow for either
    blanket coverage or per-target coverage settings. As well, an improved
    usage guide has been added to the file.

--- code-coverage.cmake
+++ code-coverage.cmake
@@ -13,6 +13,23 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+## USAGE:
+# To enable any code coverage instrumentation/targets, the single CMake option of `CODE_COVERAGE` needs
+# to be set to 'ON', either by GUI, ccmake, or on the command line.
+#
+# From this point, there are two primary methods for adding instrumentation to targets:
+# 1 - A blanket instrumentation by calling `code_coverage()`, where all targets in that directory and all
+#     subdirectories are automatically instrumented.
+# 2 - Per-target instrumentation by calling `target_code_coverage(<TARGET_NAME>)`, where the target is given
+#     and thus only that target is instrumented. This applies to both libraries and executables.
+#
+# To add coverage targets, such as calling `make ccov` to generate the actual coverage information for perusal
+# or consumption, either `target_code_coverage(<TARGET_NAME>)` or `target_auto_code_coverage(<TARGET_NAME>)`
+# needs to be called on an *executable* target.
+#
+# NOTE: To add coverage targets to an executable target, but *not* instrument it, call the macro with the
+# `NO_INSTRUMENTATION` option, such as `target_code_coverage(<TARGET_NAME> NO_INSTRUMENTATION)`
+
 if(NOT COVERAGE_ADDED)
 set(COVERAGE_ADDED ON)
 
@@ -31,14 +48,9 @@ if(CODE_COVERAGE)
     if("${CMAKE_C_COMPILER_ID}" MATCHES "(Apple)?[Cc]lang" OR "${CMAKE_CXX_COMPILER_ID}" MATCHES "(Apple)?[Cc]lang")
         message(STATUS "Building with llvm Code Coverage Tools")
 
-        # Warning/Error messages
         if(NOT LLVM_COV_PATH)
             message(FATAL_ERROR "llvm-cov not found! Aborting.")
         endif()
-
-        # set Flags
-        set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} -fprofile-instr-generate -fcoverage-mapping")
-        set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -fprofile-instr-generate -fcoverage-mapping")
 
         add_custom_target(ccov-clean
             COMMAND rm -f ${CMAKE_COVERAGE_OUTPUT_DIRECTORY}/binaries.list
@@ -72,7 +84,6 @@ if(CODE_COVERAGE)
     elseif(CMAKE_COMPILER_IS_GNUCXX)
         message(STATUS "Building with lcov Code Coverage Tools")
 
-        # Warning/Error messages
         if(NOT ($<UPPER_CASE:CMAKE_BUILD_TYPE> STREQUAL "DEBUG"))
             message(WARNING "Code coverage results with an optimized (non-Debug) build may be misleading")
         endif()
@@ -83,80 +94,107 @@ if(CODE_COVERAGE)
             message(FATAL_ERROR "genhtml not found! Aborting...")
         endif()
 
-        set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} --coverage -fprofile-arcs -ftest-coverage")
-        set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} --coverage -fprofile-arcs -ftest-coverage")
     else()
         message(FATAL_ERROR "Code coverage requires Clang or GCC. Aborting.")
     endif()
 endif()
 
-# Adds code coverage targets to the named target, named '${TARGET_NAME}-ccov'.
+endif(NOT COVERAGE_ADDED)
+
+# Adds code coverage instrumentation to a library, or instrumentation/targets for an executable target.
 #
+# Required:
 # TARGET_NAME - Name of the target to generate code coverage for.
-macro(target_add_code_coverage TARGET_NAME)
+# Optional:
+# NO_INSTRUMENTATION - Turns off code coverage instrumentation on this particular target (if an executable target). This 
+#                      is useful for adding test programs without instrumenting the test program itself.
+macro(target_code_coverage TARGET_NAME)
+    # Argument parsing
+    set(options NO_INSTRUMENTATION)
+    cmake_parse_arguments(target_executable_code_coverage "${options}" "" "" ${ARGN})
+
     if(CODE_COVERAGE)
-        if("${CMAKE_C_COMPILER_ID}" MATCHES "(Apple)?[Cc]lang" OR "${CMAKE_CXX_COMPILER_ID}" MATCHES "(Apple)?[Cc]lang")
-            add_custom_target(ccov-run-${TARGET_NAME}
-                COMMAND LLVM_PROFILE_FILE=${TARGET_NAME}.profraw $<TARGET_FILE:${TARGET_NAME}>
-                COMMAND echo "-object=$<TARGET_FILE:${TARGET_NAME}>" >> ${CMAKE_COVERAGE_OUTPUT_DIRECTORY}/binaries.list
-                COMMAND echo "${CMAKE_CURRENT_BINARY_DIR}/${TARGET_NAME}.profraw " >> ${CMAKE_COVERAGE_OUTPUT_DIRECTORY}/profraw.list
-                DEPENDS ccov-preprocessing ${TARGET_NAME}
-            )
+        # Instrumentation
+        if(NOT target_executable_code_coverage_NO_INSTRUMENTATION)
+            if("${CMAKE_C_COMPILER_ID}" MATCHES "(Apple)?[Cc]lang" OR "${CMAKE_CXX_COMPILER_ID}" MATCHES "(Apple)?[Cc]lang")
+                target_compile_options(${TARGET_NAME} PRIVATE -fprofile-instr-generate -fcoverage-mapping)
+                set_target_properties(${TARGET_NAME} PROPERTIES LINK_FLAGS "-fprofile-instr-generate -fcoverage-mapping")    
+            elseif(CMAKE_COMPILER_IS_GNUCXX)
+                target_compile_options(${TARGET_NAME} PRIVATE --coverage -fprofile-arcs -ftest-coverage)
+                target_link_libraries(gcov)
+            endif()
+        endif()
 
-            add_custom_target(ccov-processing-${TARGET_NAME}
-                COMMAND llvm-profdata merge -sparse ${TARGET_NAME}.profraw -o ${TARGET_NAME}.profdata
-                DEPENDS ccov-run-${TARGET_NAME}
-            )
+        # Targets
+        get_target_property(target_type ${TARGET_NAME} TYPE)
+        if(target_type STREQUAL "EXECUTABLE")
+            if("${CMAKE_C_COMPILER_ID}" MATCHES "(Apple)?[Cc]lang" OR "${CMAKE_CXX_COMPILER_ID}" MATCHES "(Apple)?[Cc]lang")
+                add_custom_target(ccov-run-${TARGET_NAME}
+                    COMMAND LLVM_PROFILE_FILE=${TARGET_NAME}.profraw $<TARGET_FILE:${TARGET_NAME}>
+                    COMMAND echo "-object=$<TARGET_FILE:${TARGET_NAME}>" >> ${CMAKE_COVERAGE_OUTPUT_DIRECTORY}/binaries.list
+                    COMMAND echo "${CMAKE_CURRENT_BINARY_DIR}/${TARGET_NAME}.profraw " >> ${CMAKE_COVERAGE_OUTPUT_DIRECTORY}/profraw.list
+                    DEPENDS ccov-preprocessing ${TARGET_NAME}
+                )
 
-            add_custom_target(ccov-show-${TARGET_NAME}
-                COMMAND llvm-cov show $<TARGET_FILE:${TARGET_NAME}> -instr-profile=${TARGET_NAME}.profdata -show-line-counts-or-regions
-                DEPENDS ccov-processing-${TARGET_NAME}
-            )
+                add_custom_target(ccov-processing-${TARGET_NAME}
+                    COMMAND llvm-profdata merge -sparse ${TARGET_NAME}.profraw -o ${TARGET_NAME}.profdata
+                    DEPENDS ccov-run-${TARGET_NAME}
+                )
 
-            add_custom_target(ccov-rpt-${TARGET_NAME}
-                COMMAND llvm-cov report $<TARGET_FILE:${TARGET_NAME}> -instr-profile=${TARGET_NAME}.profdata
-                DEPENDS ccov-processing-${TARGET_NAME}
-            )
+                add_custom_target(ccov-show-${TARGET_NAME}
+                    COMMAND llvm-cov show $<TARGET_FILE:${TARGET_NAME}> -instr-profile=${TARGET_NAME}.profdata -show-line-counts-or-regions
+                    DEPENDS ccov-processing-${TARGET_NAME}
+                )
 
-            add_custom_target(ccov-${TARGET_NAME}
-                COMMAND llvm-cov show $<TARGET_FILE:${TARGET_NAME}> -instr-profile=${TARGET_NAME}.profdata -show-line-counts-or-regions -output-dir=${CMAKE_COVERAGE_OUTPUT_DIRECTORY}/${TARGET_NAME}-llvm-cov -format="html"
-                DEPENDS ccov-processing-${TARGET_NAME}
-            )
+                add_custom_target(ccov-rpt-${TARGET_NAME}
+                    COMMAND llvm-cov report $<TARGET_FILE:${TARGET_NAME}> -instr-profile=${TARGET_NAME}.profdata
+                    DEPENDS ccov-processing-${TARGET_NAME}
+                )
 
-            add_custom_command(TARGET ccov-${TARGET_NAME} POST_BUILD
-                COMMAND ;
-                COMMENT "Open ${CMAKE_COVERAGE_OUTPUT_DIRECTORY}/${TARGET_NAME}-llvm-cov/index.html in your browser to view the coverage report."
-            )
+                add_custom_target(ccov-${TARGET_NAME}
+                    COMMAND llvm-cov show $<TARGET_FILE:${TARGET_NAME}> -instr-profile=${TARGET_NAME}.profdata -show-line-counts-or-regions -output-dir=${CMAKE_COVERAGE_OUTPUT_DIRECTORY}/${TARGET_NAME}-llvm-cov -format="html"
+                    DEPENDS ccov-processing-${TARGET_NAME}
+                )
 
-        elseif(CMAKE_COMPILER_IS_GNUCXX)
-            set(COVERAGE_INFO "${CMAKE_RUNTIME_OUTPUT_DIRECTORY}/${TARGET_NAME}.info")
-            set(COVERAGE_CLEANED "${coverage_info}.cleaned")
+                add_custom_command(TARGET ccov-${TARGET_NAME} POST_BUILD
+                    COMMAND ;
+                    COMMENT "Open ${CMAKE_COVERAGE_OUTPUT_DIRECTORY}/${TARGET_NAME}-llvm-cov/index.html in your browser to view the coverage report."
+                )
 
-            add_custom_target(ccov-${TARGET_NAME}
-                ${LCOV_PATH} --directory . --zerocounters
-                COMMAND $<TARGET_FILE:${TARGET_NAME}>
-                COMMAND ${LCOV_PATH} --directory . --capture --output-file ${COVERAGE_INFO}
-                COMMAND ${LCOV_PATH} --remove ${COVERAGE_INFO} 'tests/*' '/usr/*' --output-file ${COVERAGE_CLEANED}
-                COMMAND ${GENHTML_PATH} -o ${CMAKE_COVERAGE_OUTPUT_DIRECTORY}/${TARGET_NAME}-lcov ${COVERAGE_CLEANED}
-                COMMAND ${CMAKE_COMMAND} -E remove ${COVERAGE_INFO} ${COVERAGE_CLEANED}
-                DEPENDS ${TARGET_NAME}
-            )
+            elseif(CMAKE_COMPILER_IS_GNUCXX)
+                set(COVERAGE_INFO "${CMAKE_RUNTIME_OUTPUT_DIRECTORY}/${TARGET_NAME}.info")
+                set(COVERAGE_CLEANED "${coverage_info}.cleaned")
 
-            add_custom_command(TARGET ccov-${TARGET_NAME} POST_BUILD
-                COMMAND ;
-                COMMENT "Open ${CMAKE_COVERAGE_OUTPUT_DIRECTORY}/${TARGET_NAME}-lcov/index.html in your browser to view the coverage report."
-            )
+                add_custom_target(ccov-${TARGET_NAME}
+                    ${LCOV_PATH} --directory . --zerocounters
+                    COMMAND $<TARGET_FILE:${TARGET_NAME}>
+                    COMMAND ${LCOV_PATH} --directory . --capture --output-file ${COVERAGE_INFO}
+                    COMMAND ${LCOV_PATH} --remove ${COVERAGE_INFO} 'tests/*' '/usr/*' --output-file ${COVERAGE_CLEANED}
+                    COMMAND ${GENHTML_PATH} -o ${CMAKE_COVERAGE_OUTPUT_DIRECTORY}/${TARGET_NAME}-lcov ${COVERAGE_CLEANED}
+                    COMMAND ${CMAKE_COMMAND} -E remove ${COVERAGE_INFO} ${COVERAGE_CLEANED}
+                    DEPENDS ${TARGET_NAME}
+                )
+
+                add_custom_command(TARGET ccov-${TARGET_NAME} POST_BUILD
+                    COMMAND ;
+                    COMMENT "Open ${CMAKE_COVERAGE_OUTPUT_DIRECTORY}/${TARGET_NAME}-lcov/index.html in your browser to view the coverage report."
+                )
+            endif()
         endif()
     endif()
 endmacro()
 
-# Adds code coverage targets to the named target, as well as adds them to the auto
-# created ccov and ccov-report targets for easier automated running.
+# Adds code coverage instrumentation to a library, or instrumentation/targets for an executable target.
+# As well, adds the target to the auto created ccov and ccov-report targets for easier automated running.
 #
+# Required:
 # TARGET_NAME - Name of the target to generate code coverage for.
-macro(target_add_auto_code_coverage TARGET_NAME)
+# Optional:
+# NO_INSTRUMENTATION - Turns off code coverage instrumentation on this particular target (if an executable target). This 
+#                      is useful for adding test programs without instrumenting the test program itself.
+macro(target_auto_code_coverage TARGET_NAME)
     if(CODE_COVERAGE)
-        target_add_code_coverage(${TARGET_NAME})
+        target_code_coverage(${TARGET_NAME})
 
         if(NOT TARGET ccov)
             add_custom_target(ccov)
@@ -174,15 +212,14 @@ macro(target_add_auto_code_coverage TARGET_NAME)
     endif()
 endmacro()
 
-macro(remove_code_coverage)
-    if(CODE_COVERAGE)
-        # LLVM
-        string(REPLACE "-fprofile-instr-generate -fcoverage-mapping" "" CMAKE_C_FLAGS ${CMAKE_C_FLAGS})
-        string(REPLACE "-fprofile-instr-generate -fcoverage-mapping" "" CMAKE_CXX_FLAGS ${CMAKE_CXX_FLAGS})
-        # GCOV
-        string(REPLACE "--coverage -fprofile-arcs -ftest-coverage" "" CMAKE_C_FLAGS ${CMAKE_C_FLAGS})
-        string(REPLACE "--coverage -fprofile-arcs -ftest-coverage" "" CMAKE_CXX_FLAGS ${CMAKE_CXX_FLAGS})
+# Adds code coverage instrumentation to all targets in the current directory and any subdirectories. To add coverage instrumentation to only
+# specific targets, use `target_code_coverage`.
+macro(code_coverage)
+    if("${CMAKE_C_COMPILER_ID}" MATCHES "(Apple)?[Cc]lang" OR "${CMAKE_CXX_COMPILER_ID}" MATCHES "(Apple)?[Cc]lang")
+        add_compile_options(-fprofile-instr-generate -fcoverage-mapping)
+        set(CMAKE_EXE_LINKER_FLAGS "${CMAKE_EXE_LINKER_FLAGS} -fprofile-instr-generate -fcoverage-mapping")
+    elseif(CMAKE_COMPILER_IS_GNUCXX)
+        set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} --coverage -fprofile-arcs -ftest-coverage")
+        set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} --coverage -fprofile-arcs -ftest-coverage")
     endif()
 endmacro()
-
-endif(NOT COVERAGE_ADDED)

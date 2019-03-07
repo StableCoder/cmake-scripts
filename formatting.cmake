226179a5e51fe0c50d3802a0de1aa33ccc6237b8
George Cave <gcave@stablecoder.ca>
2019-03-06 20:35:33 -0500

    More changes/fixes for formatting
    First, reduced the number of targets for formatting. While previously
    each file got it's own target, while fine in makefiles, when using with
    Visual Studio or similar, this exploded the number of targets to a
    ridiculous degree.
    
    Thus, now only the specific named targets, and all the files are
    collapsed into them. As well, removed the silliness that was
    surreptitously adding prefixes to format target names. Now, only the
    given target name is used, and errors out if the name is already in use.

--- formatting.cmake
+++ formatting.cmake
@@ -32,31 +32,14 @@ endif()
 #
 # ~~~
 # Required:
-# FORMAT_TARGET_NAME - The name of the target to create. If it's a real target name,
-# then the files for it will be inherited, and the target name will have the prefix of
-# 'format_' added.
-
+# TARGET_NAME - The name of the target to create.
+#
 # Optional: ARGN - The list of targets OR files to format. Relative and absolute
 # paths are accepted.
 # ~~~
-function(clang_format FORMAT_TARGET_NAME)
+function(clang_format TARGET_NAME)
   if(CLANG_FORMAT_EXE)
-    if(TARGET ${FORMAT_TARGET_NAME})
-      get_target_property(_TARGET_TYPE ${FORMAT_TARGET_NAME} TYPE)
-      # Add target sources
-      if(NOT _TARGET_TYPE STREQUAL "INTERFACE_LIBRARY")
-        get_property(_TEMP TARGET ${FORMAT_TARGET_NAME} PROPERTY SOURCES)
-        foreach(iter IN LISTS _TEMP)
-          if(EXISTS ${iter})
-            set(FORMAT_CODE_FILES ${FORMAT_CODE_FILES} ${iter})
-          endif()
-        endforeach()
-      endif()
-
-      set(FORMAT_TARGET_NAME format_${FORMAT_TARGET_NAME})
-    endif()
-
-    # Check through the ARGN's
+    # Check through the ARGN's, determine existant files
     foreach(item IN LISTS ARGN)
       if(TARGET ${item})
         # If the item is a target, then we'll attempt to grab the associated
@@ -66,41 +49,37 @@ function(clang_format FORMAT_TARGET_NAME)
           get_property(_TEMP TARGET ${item} PROPERTY SOURCES)
           foreach(iter IN LISTS _TEMP)
             if(EXISTS ${iter})
-              set(FORMAT_CODE_FILES ${FORMAT_CODE_FILES} ${iter})
+              set(FORMAT_FILES ${FORMAT_FILES} ${iter})
             endif()
           endforeach()
         endif()
       elseif(EXISTS ${item})
         # Check if it's a full file path
-        set(FORMAT_CODE_FILES ${FORMAT_CODE_FILES} ${item})
+        set(FORMAT_FILES ${FORMAT_FILES} ${item})
       elseif(EXISTS ${CMAKE_CURRENT_SOURCE_DIR}/${item})
         # Check if it's based on the current source dir
-        set(FORMAT_CODE_FILES ${FORMAT_CODE_FILES}
-            ${CMAKE_CURRENT_SOURCE_DIR}/${item})
+        set(FORMAT_FILES ${FORMAT_FILES} ${CMAKE_CURRENT_SOURCE_DIR}/${item})
       endif()
     endforeach()
 
-      # Make actual targets
-    foreach(item IN LISTS FORMAT_CODE_FILES)
-      string(MD5 HASH ${item})
-
-      if(NOT TARGET format-${HASH})
-        add_custom_target(format-${HASH}
-                          COMMAND clang-format -i -style=file ${item})
-
-        if(NOT TARGET ${FORMAT_TARGET_NAME})
-          add_custom_target(${FORMAT_TARGET_NAME})
-        endif()
-
-        add_dependencies(${FORMAT_TARGET_NAME} format-${HASH})
+    # Make the target
+    if(FORMAT_FILES)
+      if(TARGET ${TARGET_NAME})
+        message(
+          ERROR
+          "Cannot create clang-format target '${TARGET_NAME}', already exists.")
+      else()
+        add_custom_target(${TARGET_NAME}
+                          COMMAND ${CLANG_FORMAT_EXE} -i -style=file
+                                  ${FORMAT_FILES})
 
         if(NOT TARGET format)
           add_custom_target(format)
         endif()
 
-        add_dependencies(format ${FORMAT_TARGET_NAME})
+        add_dependencies(format ${TARGET_NAME})
       endif()
-    endforeach()
+    endif()
 
   endif()
 endfunction()
@@ -117,38 +96,41 @@ endif()
 
 # When called, this function will call 'cmake-format' program on all listed
 # files (if both the program and the files exist and are found)
-#
-# Each individual file is created as a separate target `cmake-format-<MD5>` hash
-# of the file, and all these individual targets are then linked in as
-# dependencies of the `cmake-format` final target.
 # ~~~
+# Required:
+# TARGET_NAME - The name of the target to create.
+#
 # Optional:
 # ARGN - Any  arguments passed in will be considered as 'files' to perform the
 # formatting on. Any items that are not files will be ignored. Both relative and
 # absolute paths are accepted.
 # ~~~
-function(cmake_format)
+function(cmake_format TARGET_NAME)
   if(CMAKE_FORMAT_EXE)
+    # Determine files that exist
     foreach(iter IN LISTS ARGN)
       if(EXISTS ${iter})
-        set(FORMAT_PATH ${iter})
+        set(FORMAT_FILES ${FORMAT_FILES} ${iter})
       elseif(EXISTS ${CMAKE_CURRENT_SOURCE_DIR}/${iter})
-        set(FORMAT_PATH ${CMAKE_CURRENT_SOURCE_DIR}/${iter})
-      endif()
-
-      if(FORMAT_PATH)
-        string(MD5 HASH ${FORMAT_PATH})
-
-        if(NOT TARGET cmake-format-${HASH})
-          add_custom_target(cmake-format-${HASH}
-                            COMMAND cmake-format -i ${FORMAT_PATH})
-
-          if(NOT TARGET cmake-format)
-            add_custom_target(cmake-format)
-          endif()
-          add_dependencies(cmake-format cmake-format-${HASH})
-        endif()
+        set(FORMAT_FILES ${FORMAT_FILES} ${CMAKE_CURRENT_SOURCE_DIR}/${iter})
       endif()
     endforeach()
+
+    # Generate target
+    if(FORMAT_FILES)
+      if(TARGET ${TARGET_NAME})
+        message(
+          ERROR
+          "Cannot create cmake-format target '${TARGET_NAME}', already exists.")
+      else()
+        add_custom_target(${TARGET_NAME}
+                          COMMAND ${CMAKE_FORMAT_EXE} -i ${FORMAT_FILES})
+
+        if(NOT TARGET cmake-format)
+          add_custom_target(cmake-format)
+        endif()
+        add_dependencies(cmake-format ${TARGET_NAME})
+      endif()
+    endif()
   endif()
 endfunction()

c1a621af7f7b768257d8995215ef651b90702634
George Cave <gcave@stablecoder.ca>
2023-07-22 13:35:41 -0400

    Add C variables for clang-tidy/iwyu and cppcheck
    The tools.cmake file included a number of tools, but only implemented
    their use just in C++.
    
    This adds their use with C.

--- tools.cmake
+++ tools.cmake
@@ -1,5 +1,5 @@
 #
-# Copyright (C) 2018-2020 by George Cave - gcave@stablecoder.ca
+# Copyright (C) 2018-2023 by George Cave - gcave@stablecoder.ca
 #
 # Licensed under the Apache License, Version 2.0 (the "License"); you may not
 # use this file except in compliance with the License. You may obtain a copy of
@@ -21,6 +21,9 @@ option(CPPCHECK "Turns on cppcheck processing if it is found." OFF)
 # as the options set.
 macro(clang_tidy)
   if(CLANG_TIDY AND CLANG_TIDY_EXE)
+    set(CMAKE_C_CLANG_TIDY
+        ${CLANG_TIDY_EXE} ${ARGN}
+        CACHE STRING "" FORCE)
     set(CMAKE_CXX_CLANG_TIDY
         ${CLANG_TIDY_EXE} ${ARGN}
         CACHE STRING "" FORCE)
@@ -31,6 +34,9 @@ endmacro()
 # used as the options set.
 macro(include_what_you_use)
   if(IWYU AND IWYU_EXE)
+    set(CMAKE_C_INCLUDE_WHAT_YOU_USE
+        ${IWYU_EXE} ${ARGN}
+        CACHE STRING "" FORCE)
     set(CMAKE_CXX_INCLUDE_WHAT_YOU_USE
         ${IWYU_EXE} ${ARGN}
         CACHE STRING "" FORCE)
@@ -41,6 +47,9 @@ endmacro()
 # options set.
 macro(cppcheck)
   if(CPPCHECK AND CPPCHECK_EXE)
+    set(CMAKE_C_CPPCHECK
+        ${CPPCHECK_EXE} ${ARGN}
+        CACHE STRING "" FORCE)
     set(CMAKE_CXX_CPPCHECK
         ${CPPCHECK_EXE} ${ARGN}
         CACHE STRING "" FORCE)
@@ -53,17 +62,26 @@ if(CLANG_TIDY_EXE)
   message(STATUS "clang-tidy found: ${CLANG_TIDY_EXE}")
   if(NOT CLANG_TIDY)
     message(STATUS "clang-tidy NOT ENABLED via 'CLANG_TIDY' variable!")
+    set(CMAKE_C_CLANG_TIDY
+        ""
+        CACHE STRING "" FORCE) # delete it
     set(CMAKE_CXX_CLANG_TIDY
         ""
         CACHE STRING "" FORCE) # delete it
   endif()
 elseif(CLANG_TIDY)
   message(SEND_ERROR "Cannot enable clang-tidy, as executable not found!")
+  set(CMAKE_C_CLANG_TIDY
+      ""
+      CACHE STRING "" FORCE) # delete it
   set(CMAKE_CXX_CLANG_TIDY
       ""
       CACHE STRING "" FORCE) # delete it
 else()
   message(STATUS "clang-tidy not found!")
+  set(CMAKE_C_CLANG_TIDY
+      ""
+      CACHE STRING "" FORCE) # delete it
   set(CMAKE_CXX_CLANG_TIDY
       ""
       CACHE STRING "" FORCE) # delete it
@@ -75,6 +93,9 @@ if(IWYU_EXE)
   message(STATUS "include-what-you-use found: ${IWYU_EXE}")
   if(NOT IWYU)
     message(STATUS "include-what-you-use NOT ENABLED via 'IWYU' variable!")
+    set(CMAKE_C_INCLUDE_WHAT_YOU_USE
+        ""
+        CACHE STRING "" FORCE) # delete it
     set(CMAKE_CXX_INCLUDE_WHAT_YOU_USE
         ""
         CACHE STRING "" FORCE) # delete it
@@ -82,11 +103,17 @@ if(IWYU_EXE)
 elseif(IWYU)
   message(
     SEND_ERROR "Cannot enable include-what-you-use, as executable not found!")
+  set(CMAKE_C_INCLUDE_WHAT_YOU_USE
+      ""
+      CACHE STRING "" FORCE) # delete it
   set(CMAKE_CXX_INCLUDE_WHAT_YOU_USE
       ""
       CACHE STRING "" FORCE) # delete it
 else()
   message(STATUS "include-what-you-use not found!")
+  set(CMAKE_C_INCLUDE_WHAT_YOU_USE
+      ""
+      CACHE STRING "" FORCE) # delete it
   set(CMAKE_CXX_INCLUDE_WHAT_YOU_USE
       ""
       CACHE STRING "" FORCE) # delete it
@@ -97,23 +124,35 @@ mark_as_advanced(FORCE CPPCHECK_EXE)
 if(CPPCHECK_EXE)
   message(STATUS "cppcheck found: ${CPPCHECK_EXE}")
   if(CPPECHECK)
+    set(CMAKE_C_CPPCHECK
+        "${CPPCHECK_EXE};--enable=warning,performance,portability,missingInclude;--template=\"[{severity}][{id}] {message} {callstack} \(On {file}:{line}\)\";--suppress=missingIncludeSystem;--quiet;--verbose;--force"
+    )
     set(CMAKE_CXX_CPPCHECK
         "${CPPCHECK_EXE};--enable=warning,performance,portability,missingInclude;--template=\"[{severity}][{id}] {message} {callstack} \(On {file}:{line}\)\";--suppress=missingIncludeSystem;--quiet;--verbose;--force"
     )
   endif()
   if(NOT CPPCHECK)
     message(STATUS "cppcheck NOT ENABLED via 'CPPCHECK' variable!")
+    set(CMAKE_C_CPPCHECK
+        ""
+        CACHE STRING "" FORCE) # delete it
     set(CMAKE_CXX_CPPCHECK
         ""
         CACHE STRING "" FORCE) # delete it
   endif()
 elseif(CPPCHECK)
   message(SEND_ERROR "Cannot enable cppcheck, as executable not found!")
+  set(CMAKE_C_CPPCHECK
+      ""
+      CACHE STRING "" FORCE) # delete it
   set(CMAKE_CXX_CPPCHECK
       ""
       CACHE STRING "" FORCE) # delete it
 else()
   message(STATUS "cppcheck not found!")
+  set(CMAKE_C_CPPCHECK
+      ""
+      CACHE STRING "" FORCE) # delete it
   set(CMAKE_CXX_CPPCHECK
       ""
       CACHE STRING "" FORCE) # delete it
